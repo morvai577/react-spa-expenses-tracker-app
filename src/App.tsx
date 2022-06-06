7f3b9a585b5048b56c0eb51d5cb28b23fe2cf35b
morvai577 <thevaimor@icloud.com>
2022-06-06 14:41:16 +1200

    Included new expense component to be rendered on app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Expenses } from "./components/Expenses/Expenses";
+import { NewExpense } from "@/components/NewExpense/NewExpense";
 
 export default function App() {
   const expenses = [
@@ -25,7 +26,7 @@ export default function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense></NewExpense>
       <Expenses items={expenses} />
     </div>
   );
